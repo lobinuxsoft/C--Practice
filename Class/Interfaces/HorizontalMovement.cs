7992fa089d1b7556377bfe4025a8952e75e414a0
lobinux <lobinuxsoft@gmail.com>
2022-05-17 12:08:10 -0300

    Implement partial class Horizontal Movement

--- Class/Interfaces/HorizontalMovement.cs
+++ Class/Interfaces/HorizontalMovement.cs
@@ -5,6 +5,7 @@ namespace Game.Class.Interfaces
 {
     public class HorizontalMovement : IMovement
     {
+        int hMoveValue = 0;
         public Vector2 Move(Vector2 position, Random rnd)
         {
             throw new NotImplementedException();
